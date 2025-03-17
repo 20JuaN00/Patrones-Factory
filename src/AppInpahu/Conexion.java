84ad3c2e3a6ce43e39b6b51f83531213700381f9
20JuaN00 <20juandiego00@gmail.com>
2025-03-17 09:56:21 -0500

    Patrones Factoryy

---
+++ src/AppInpahu/Conexion.java
@@ -0,0 +1,17 @@
+
+package AppInpahu;
+
+
+public class Conexion {
+
+    Private static Conexion instancia;
+    
+    Private Conexion (){
+    
+    }
+    
+    public static Conexion getInstancia(){
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/UNINPAHUbd/NewMain.java
@@ -0,0 +1,14 @@
+
+package UNINPAHUbd;
+
+
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
